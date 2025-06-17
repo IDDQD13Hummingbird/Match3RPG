f0a99a612aaebda90fe3c06a70b7c0c53933c83d
Candidate_514_23DPRO101 <splunch18@gmail.com>
2025-06-17 15:47:18 +0200

    fixing icon generation - not working

--- Assets/_Scripts/Bia/Board.cs
+++ Assets/_Scripts/Bia/Board.cs
@@ -5,10 +5,15 @@ public class Board : MonoBehaviour
 public int height;
 public GameObject TilePrefab;
 private Tile[,] allTiles;
+
+public GameObject[] allowedIcons;
+private int[,] tileTypes;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         allTiles = new Tile[width, height];
+        tileTypes = new int[width, height];
         CreateBoard();
     }
 
@@ -22,9 +27,51 @@ private Tile[,] allTiles;
                 GameObject tempTile = Instantiate(TilePrefab, tempPosition, Quaternion.identity);
                 tempTile.transform.parent = this.transform;
                 tempTile.name = "(" + i + ", " + j + ")";
+
+                Tile tileComponent = tempTile.GetComponent<Tile>();
+                allTiles[i,j]=tileComponent;
+
+                int safeIconType = GetSafeIconType(i, j);
+                tileTypes[i,j]=safeIconType;
+
+                tileComponent.InitializeByType(safeIconType,allowedIcons);
             }
         }
 
+    }
+
+    private int GetSafeIconType(int x, int y)
+    {
+        for (int i = 0; i < allowedIcons.Length; i++)
+        {
+            if (IsSafeToCreate(x, y,i))
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    bool IsSafeToCreate(int x,int y,int Type)
+    {
+        if (x >= 2)    //horisontal check
+        {
+            if (tileTypes[x - 1, y] == Type && tileTypes[x - 2, y] == Type)
+            {
+                return false;
+            }
+        }
+
+        if (y >= 2)    //horisontal check
+        {
+            if (tileTypes[x , y-1] == Type && tileTypes[x, y-2] == Type)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     // Update is called once per frame
--- Assets/_Scripts/Bia/Tile.cs
+++ Assets/_Scripts/Bia/Tile.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public GameObject[] icons;
+    private int tileType=-1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Initialize();
+        if (tileType == -1)
+        {
+            Initialize();
+        }
+    }
+
+    public int getTileType()
+    {
+        return tileType;
     }
 
     // Update is called once per frame
@@ -19,7 +28,25 @@ public class Tile : MonoBehaviour
     {
         int IconToUse = Random.Range(0, icons.Length);
         GameObject icon = Instantiate(icons[IconToUse], transform.position, Quaternion.identity);
-        icon.transform.parent = this.transform;
-        icon.name = this.gameObject.name;
+        //icon.transform.parent = this.transform;
+        //icon.name = this.gameObject.name;
+    }
+
+    public void InitializeByType(int Type, GameObject[] IconsArray)
+    {
+        tileType = Type;
+        icons = IconsArray;
+        CreateIcon(Type);
+    }
+
+    private void CreateIcon(int Type)
+    {
+        //int IconToUse = Random.Range(0, icons.Length);
+        if (Type >= 0 && Type < icons.Length)
+        {
+            GameObject icon = Instantiate(icons[Type], transform.position, Quaternion.identity);
+            icon.transform.parent = this.transform;
+            icon.name = this.gameObject.name;
+        }  
     }
 }
