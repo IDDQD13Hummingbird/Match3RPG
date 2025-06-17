d2a751a3731dff59d512b4d8ed87b3deb150323a
IDDQD_Hummingbird <IDDQD13Workshop@gmail.com>
2025-06-17 17:21:40 +0200

    Started initializing heroes!
    Revisit public RPG_stats initialization

--- Assets/_Scripts/Harpy/Heroes.cs
+++ Assets/_Scripts/Harpy/Heroes.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Heroes : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public RPG_Stats Barbarian = new RPG_Stats("Barbarian", float health, float damage, int speed, int level);
+    public RPG_Stats Healer = new RPG_Stats("Healer", float health, float damage, int speed, int level);
+    public RPG_Stats Rogue = new RPG_Stats("Rogue", float health, float damage, int speed, int level);
+    public RPG_Stats Wizard = new RPG_Stats("Wizard", float health, float damage, int speed, int level);
+
+
+    /// <summary>
+    /// We need a separate formula for calculating each hero's stats?
+    /// And when they have different abilities, that needs to be stored somewhere. Enum of sorts?
+    /// </summary>
+
+    // When you start - heroes lvl 1, stats generate differently based on hero class?
+    // When you load back into the game - current hero level and stats preserved.
+
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Barbarian.DisplayInfo();
+        Healer.DisplayInfo();
+        Rogue.DisplayInfo();
+        Wizard.DisplayInfo();
     }
 }
