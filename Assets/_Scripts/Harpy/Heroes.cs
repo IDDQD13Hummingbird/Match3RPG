15bd7ad538bf4f710a9de79f163b6d76797585fb
Aedan Soellaart <aedansoellaart@gmail.com>
2025-06-17 17:32:14 +0200

    removed errors

--- Assets/_Scripts/Harpy/Heroes.cs
+++ Assets/_Scripts/Harpy/Heroes.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Heroes : MonoBehaviour
 {
-    public RPG_Stats Barbarian = new RPG_Stats("Barbarian", float health, float damage, int speed, int level);
-    public RPG_Stats Healer = new RPG_Stats("Healer", float health, float damage, int speed, int level);
-    public RPG_Stats Rogue = new RPG_Stats("Rogue", float health, float damage, int speed, int level);
-    public RPG_Stats Wizard = new RPG_Stats("Wizard", float health, float damage, int speed, int level);
+    // public RPG_Stats Barbarian = new RPG_Stats("Barbarian", float health, float damage, int speed, int level);
+    // public RPG_Stats Healer = new RPG_Stats("Healer", float health, float damage, int speed, int level);
+    // public RPG_Stats Rogue = new RPG_Stats("Rogue", float health, float damage, int speed, int level);
+    // public RPG_Stats Wizard = new RPG_Stats("Wizard", float health, float damage, int speed, int level);
 
 
     /// <summary>
@@ -19,9 +19,9 @@ public class Heroes : MonoBehaviour
 
     void Start()
     {
-        Barbarian.DisplayInfo();
-        Healer.DisplayInfo();
-        Rogue.DisplayInfo();
-        Wizard.DisplayInfo();
+        // Barbarian.DisplayInfo();
+        // Healer.DisplayInfo();
+        // Rogue.DisplayInfo();
+        // Wizard.DisplayInfo();
     }
 }
